c32dcb4c7c4079de6d4066462dacc564ceb25b7b
VoVietBac <bac24062002@gmail.com>
2025-10-19 20:58:44 +0700

    #5.0

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,11 @@
+import mysql from "mysql2";
+
+const connection = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    database: "jwt",
+});
+
 const handleHelloWorld = (req, res) => {
     return res.render("home");
 };
@@ -6,5 +14,25 @@ const handleUserPage = (req, res) => {
     return res.render("user");
 };
 
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.emailName;
+    let password = req.body.passwordName;
+    let username = req.body.usernameName;
 
-module.exports = { handleHelloWorld, handleUserPage };
+    connection.query(
+        "insert into `users`(`email`, `password`, `username`) values (?, ?, ?);",
+        [email, password, username],
+        (err, results, fields) => {
+            if (err) {
+                console.error("Error inserting user:", err);
+                return res.status(500).send("Error creating user.");
+            }
+            console.log(results);
+            console.log(fields);
+            // return res.send("User created successfully.");
+        }
+    );
+    return res.send("User creation logic not implemented yet.", req);
+};
+
+module.exports = { handleHelloWorld, handleUserPage, handleCreateNewUser };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 import configViewEngine from "./configs/viewEngine.js";
 import initWebRoutes from "./routes/web.js";
+
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +11,10 @@ const PORT = process.env.PORT || 8080;
 
 //config view engine
 configViewEngine(app);
+
+//config body parser to read data from req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //init web routes
 initWebRoutes(app);
